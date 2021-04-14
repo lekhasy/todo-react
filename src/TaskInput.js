86a2cb624be604566f6d9ea028886a876f32a7e9
Vivian <vietlehoang2000@gmail.com>
2021-04-14 20:24:16 +0700

    prettier implment

--- src/TaskInput.js
+++ src/TaskInput.js
@@ -1,22 +1,29 @@
 import { Input } from "antd";
 import React from "react";
 
-function TaskInput({handleAddTask}) {
-  const [inputValue,setValue] = React.useState("");
-  
+function TaskInput({ handleAddTask }) {
+  const [inputValue, setValue] = React.useState("");
+
   const handlePressEnter = () => {
-    if(inputValue === ""){
+    if (inputValue === "") {
       return;
     }
     handleAddTask(inputValue);
     setValue("");
   };
 
-  const handleOnChange = (e) =>{
+  const handleOnChange = (e) => {
     setValue(e.target.value);
-  }
+  };
 
-  return <Input placeholder="Nhập tên task rồi ấn enter" value={inputValue} onChange={handleOnChange} onPressEnter={handlePressEnter} />;
+  return (
+    <Input
+      placeholder="Nhập tên task rồi ấn enter"
+      value={inputValue}
+      onChange={handleOnChange}
+      onPressEnter={handlePressEnter}
+    />
+  );
 }
 
 export default TaskInput;

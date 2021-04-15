6391ed1a8943f7e430da2bc13f82ea4a46bbdb01
LinkK9 <linhgtx@gmail.com>
2021-04-15 13:32:34 +0700

    learn git

--- src/App.js
+++ src/App.js
@@ -51,13 +51,13 @@ function App() {
         <TodoList
           changeStatus={changeStatus}
           taskList={tasksNotCompleted}
-          title={"Danh sách tasks"}
+          title={"Danh sách task"}
         />
 
         <TodoList
           changeStatus={changeStatus}
           taskList={tasksCompleted}
-          title={"Danh sách tasks hoàn thành"}
+          title={"Danh sách task hoàn thành"}
         />
       </section>
     </div>

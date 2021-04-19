4353fac0658002aea88b53253b558b844a3c9001
sy1lk <sy1lk@vn.ibm.com>
2021-04-19 18:59:32 +0700

    fix test fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 import App from "./App";
+import Store from "./redux/Store";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/Todo app/i);
   expect(linkElement).toBeInTheDocument();
 });

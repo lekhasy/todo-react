6a22ad762031f11605f4c2a2f12d5952f8a25b84
sy1lk <sy1lk@vn.ibm.com>
2021-04-19 18:34:38 +0700

    fix test fail

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {
   ChooseFavouriteTask,
 } from "./redux/ActionCreator";
 
-export const TodoAppConText = React.createContext();
+export const TodoAppConText = React.createContext({ appName: "Default App Name" });
 
 function App() {
   const dispatch = useDispatch();
